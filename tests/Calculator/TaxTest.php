7719ba051c4bdfae7e81ca5437470ee7a4a67395
Laurent Bassin <laurent@bassin.info>
2018-11-21 10:33:15 +0100

    Add phpunit

--- tests/Calculator/TaxTest.php
+++ tests/Calculator/TaxTest.php
@@ -3,8 +3,7 @@
 namespace App\Tests\Calculator;
 
 use App\Calculator\Tax as TaxCalculator;
-use App\Entity\SAS;
-use App\Entity\SelfEmployed;
+use App\Entity\Taxable;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
@@ -18,25 +17,7 @@ class TaxTest extends TestCase
         $this->calculator = new TaxCalculator();
     }
 
-    private function getSAS()
-    {
-        /** @var SAS|MockObject $company */
-        $company = $this->createMock(SAS::class);
-        $company->method('getTaxPercentage')->willReturn(33);
-
-        return $company;
-    }
-
-    private function getSelfEmployed()
-    {
-        /** @var SAS|MockObject $company */
-        $company = $this->createMock(SelfEmployed::class);
-        $company->method('getTaxPercentage')->willReturn(25);
-
-        return $company;
-    }
-
-    public function SASCorrectTaxes()
+    public function correctTaxes()
     {
         return [
             [100, 33],
@@ -45,51 +26,25 @@ class TaxTest extends TestCase
         ];
     }
 
-    public function SelfEmployedCorrectTaxes()
-    {
-        return [
-            [100, 25],
-            [74623, 18655.75],
-            [6, 1.5],
-        ];
-    }
-
     /**
-     * @dataProvider SASCorrectTaxes
+     * @dataProvider correctTaxes
      */
-    public function testSASCorrectCase($earning, $taxExpected)
+    public function testCorrectCase($earning, $taxExpected)
     {
-        $company = $this->getSAS();
+        /** @var Taxable|MockObject $company */
+        $company = $this->createMock(Taxable::class);
+        $company->expects($this->once())->method('getTaxPercentage')->willReturn('33');
 
         $tax = $this->calculator->calcul($company, $earning);
 
         $this->assertEquals($taxExpected, $tax);
     }
 
-    public function testSASEarningLessThanZero()
+    public function testEarningLessThanZero()
     {
-        $company = $this->getSAS();
-
-        $this->expectException(\LogicException::class);
-
-        $this->calculator->calcul($company, -1);
-    }
-
-    /**
-     * @dataProvider SelfEmployedCorrectTaxes
-     */
-    public function testSelfEmployedCorrectCase($earning, $taxExpected)
-    {
-        $company = $this->getSelfEmployed();
-
-        $tax = $this->calculator->calcul($company, $earning);
-
-        $this->assertEquals($taxExpected, $tax);
-    }
-
-    public function testSelfEmployedEarningLessThanZero()
-    {
-        $company = $this->getSelfEmployed();
+        /** @var Taxable|MockObject $company */
+        $company = $this->createMock(Taxable::class);
+        $company->expects($this->never())->method('getTaxPercentage');
 
         $this->expectException(\LogicException::class);
 
