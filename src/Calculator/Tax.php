013595f1f25d718d486bf31441bbc38538e26318
Laurent Bassin <laurent@bassin.info>
2018-11-21 09:57:11 +0100

    Add tax calculator

---
+++ src/Calculator/Tax.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Calculator;
+
+use App\Entity\Taxable;
+
+class Tax
+{
+    public function calcul(Taxable $taxable, float $annualEarnings): float
+    {
+        if ($annualEarnings < 0) {
+            throw new \LogicException('Annual earning has to be greater than 0');
+        }
+
+        return $annualEarnings * $taxable->getTaxPercentage();
+    }
+}
